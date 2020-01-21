03f8407ea9b5b72b8e42c3aa4d7458add41911bc
Vivek Ranjan <vivekranjan92@gmail.com>
2020-01-21 14:45:11 +0530

    SpecialElementAlertBox
    Script to generate the alert box using selenium C#

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -3,45 +3,42 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
 
-class SelectElementDropDownMenu
+class SpecialElementAlertBox
 {
     static IWebDriver driver = new ChromeDriver();
-    static IWebElement dropDownMenu;
-    static IWebElement elementFromTheDropDownMenu;
+    static IAlert alert;
+    static IWebElement image;
 
     static void Main()
     {
-        string url = "http://testing.todorvachev.com/special-elements/drop-down-menu-test/";
-        // we can switch the number from 1 to 4(since we have only 4 value in the dropdown) to get the next dropdown element
-        // here we are only checking the value of 3rd element in the drop down list
-        string dropDownMenuElements = "#post-6 > div > p:nth-child(6) > select > option:nth-child(3)";
+        string url = "http://testing.todorvachev.com/special-elements/alert-box/";
 
         driver.Navigate().GoToUrl(url);
 
-        dropDownMenu = driver.FindElement(By.Name("DropDownTest"));
+        // Initializing the alert
+        alert = driver.SwitchTo().Alert();
 
-        // this will give us the current selected value from the dropdown
-        Console.WriteLine("The selected value is: " + dropDownMenu.GetAttribute("value"));
+        // to get the text out of the alert
+        Console.WriteLine(alert.Text);
 
-        elementFromTheDropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuElements));
-        // this will select the 3rd option from the dropdown and print it on the console window
-        Console.WriteLine("The third option from the drop down menu is: " + elementFromTheDropDownMenu.GetAttribute("value"));
+        // when click on ok to dismiss the alert, we have to make use of Accept()
+        alert.Accept();
 
-        elementFromTheDropDownMenu.Click();
+        image = driver.FindElement(By.CssSelector("#post-119 > div > figure > img"));
 
-        Console.WriteLine("The selected value is: " + dropDownMenu.GetAttribute("value"));
-        Thread.Sleep(3000);
-
-        // we are looping through to check all the value in the dropdown
-        for (int i = 1; i <= 4; i++)
+        try
         {
-            dropDownMenuElements = "#post-6 > div > p:nth-child(6) > select > option:nth-child(" + i + ")";
-            elementFromTheDropDownMenu = driver.FindElement(By.CssSelector(dropDownMenuElements));
-            // it will give us the number of current option, and we can verify if it is correct or not
-            Console.WriteLine("The " + i + " option from the drop down menu is: " + elementFromTheDropDownMenu.GetAttribute("value"));
+            if (image.Displayed)
+            {
+                Console.WriteLine("The alert was successfuly accepted and I can see the image!");
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            Console.WriteLine("Something went wrong!!");
         }
 
-        Thread.Sleep(15000);
+        Thread.Sleep(3000);
 
         driver.Quit();
     }
